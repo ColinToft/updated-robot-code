b2b4d0bc9e1ced314417a46f24bd6ac041835223
Colin Toft <47548563+ColinToft@users.noreply.github.com>
2022-11-26 12:41:26 -0500

    Update FloatableSolenoid.java

--- src/main/java/frc/robot/util/FloatableSolenoid.java
+++ src/main/java/frc/robot/util/FloatableSolenoid.java
@@ -1,7 +1,5 @@
 package frc.robot.util;
 
-import edu.wpi.first.wpilibj2.command.*;
-
 /**
  * Using two double solenoids to allow floating of piston.
  * @author Marston Connell
@@ -11,14 +9,10 @@ public class FloatableSolenoid{
     private DoubleSolenoidToggler one;
     private DoubleSolenoidToggler two;
     private boolean extended = false;
-    private Subsystem subsystem;
 
-    public FloatableSolenoid(Subsystem subsystem, int portOne, int portTwo, int portThree, int portFour) {
+    public FloatableSolenoid(int portOne, int portTwo, int portThree, int portFour) {
         one = new DoubleSolenoidToggler(portOne, portTwo);
         two = new DoubleSolenoidToggler(portThree, portFour);
-
-        one.setOff();
-        this.subsystem = subsystem;
     }
 
     public void floatPiston(){
@@ -27,30 +21,23 @@ public class FloatableSolenoid{
     }
 
     public void retractPiston() {
-        two.setOff();
+        two.retract();
         one.extend();
         extended = false;
     }
 
     public void extendPiston() {
-        one.setOff();
+        one.retract();
         two.extend();
         extended = true;
     }
 
-    /** Returns a {@link SimpleCommand} that retracts this double solenoid. */
-    public Command retractCommand() {
-        return new SequentialCommandGroup(new InstantCommand(() -> floatPiston(), subsystem), new WaitCommand(0.1), new InstantCommand(() -> retractPiston()));
-    }
-
-    //** Returns a {@link SimpleCommand} that retracts this double solenoid. */
-    public Command extendCommand() {
-        return new SequentialCommandGroup(new InstantCommand(() -> floatPiston(), subsystem), new WaitCommand(0.1), new InstantCommand(() -> extendPiston()));
-    }
-
-    /** Returns a {@link SimpleCommand} that toggles this double solenoid. */
-    public Command toggleCommand() {
-        return new ConditionalCommand(retractCommand(), extendCommand(), this::isExtended);
+    public void togglePiston() {
+        if (extended) {
+            retractPiston();
+        } else {
+            extendPiston();
+        }
     }
 
     public boolean isExtended() {
